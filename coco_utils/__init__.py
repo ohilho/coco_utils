19f8c29d322747a9ce3479cf92f89a7e29d62a81
ohilho <iroiro515@gmail.com>
2021-08-12 02:23:02 +0900

    add __init__.py. add all public modules and thier functions

---
+++ coco_utils/__init__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+
+from .common import yes_no, make_dim_3, save, load, is_image
+from .concatenate import concatenate
+from .inspect import inspect
+from .refine import refine
+from .rename import rename
+from .resize import resize_image
+from .select_channel import select_channel
+from .statistics import mean_std
+from .type_cast import type_cast
+
+__all__ = ['yes_no', 'make_dim_3', 'save', 'load', 'concatenate', 'is_image', 'inspect',
+           'refine', 'rename', 'resize_image', 'select_channel', 'mean_std', 'type_cast']
